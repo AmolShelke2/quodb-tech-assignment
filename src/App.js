6a49660417086a3596e65ab9f1e4fd698210542b
AmolShelke2 <shelkeamol897@gmail.com>
2024-03-30 13:40:11 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import AddTodos from "./components/AddTodos";
 
 function App() {
   return (
-    <div className="h-screen w-full bg-slate-200 flex flex-col justify-center items-center">
-      <div className="h-[600px] w-[600px] bg-white shadow-md rounded-xl relative">
+    <div className="sm:h-screen w-full bg-slate-200 flex flex-col justify-center items-center">
+      <div className="h-full sm:h-[600px] w-full sm:w-[600px] bg-white shadow-md rounded-xl relative">
         <h2 className="text-gray-800 font-medium text-xl p-2 border-b border">
           My Todo-List
         </h2>
